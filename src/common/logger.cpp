88d2c9d63af031859cba1cdc2667e62bbc68bded
Adriaan de Groot <groot@kde.org>
2015-10-04 10:56:07 +0200

    License: add license to all C++ files

--- src/common/logger.cpp
+++ src/common/logger.cpp
@@ -1,3 +1,10 @@
+/*
+Copyright (c) 2014, 2015 InternetWide.org and the ARPA2.net project
+All rights reserved. See file LICENSE for exact terms (2-clause BSD license).
+
+Adriaan de Groot <groot@kde.org>
+*/
+
 #include "logger.h"
 
 int Steamworks::LoggerManager::instances = 0;
