02ea47212dd44815aea11a7ec830249ec2d04ab0
Adriaan de Groot <groot@kde.org>
2015-10-04 23:18:42 +0200

    CMake: use appropriate compiler flag for C++11

---
+++ cmake/CheckCXX11Flags.cmake
@@ -0,0 +1,15 @@
+# Check which flag to pass in for C++11 or C++0x support.
+#
+# Source: http://www.guyrutenberg.com/2014/01/05/enabling-c11-c0x-in-cmake/
+# License: not listed
+#
+include(CheckCXXCompilerFlag)
+CHECK_CXX_COMPILER_FLAG("-std=c++11" COMPILER_SUPPORTS_CXX11)
+CHECK_CXX_COMPILER_FLAG("-std=c++0x" COMPILER_SUPPORTS_CXX0X)
+if(COMPILER_SUPPORTS_CXX11)
+	set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11")
+elseif(COMPILER_SUPPORTS_CXX0X)
+	set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++0x")
+else()
+        message(FATAL "The compiler ${CMAKE_CXX_COMPILER} has no C++11 support. Please use a different C++ compiler.")
+endif()
