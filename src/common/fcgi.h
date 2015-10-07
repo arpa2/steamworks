37a0f4f09b9c5b7f025536f0661891ac31ff7fc8
Adriaan de Groot <groot@kde.org>
2015-10-07 10:45:18 +0200

    APIDocs: FCGI and Logging.

--- src/common/fcgi.h
+++ src/common/fcgi.h
@@ -5,6 +5,15 @@ All rights reserved. See file LICENSE for exact terms (2-clause BSD license).
 Adriaan de Groot <groot@kde.org>
 */
 
+/**
+ * FCGI support code. An FCGI program waits for incoming connections
+ * (corresponding to a new FCGI request) and processes them. The API
+ * is just one function, mainloop(), which uses a passed-in dispatcher
+ * object to respond to the requests.
+ * 
+ * Use init_logging() to cause FCGI processing to log to a given
+ * category; this is optional.
+ */
 #include "verb.h"
 
 namespace Steamworks
@@ -12,7 +21,10 @@ namespace Steamworks
 namespace FCGI
 {
 
-int init_logging(const std::string& logname); 
+/// Set up logging, use the given @p categoryname for logging FCGI actions.
+int init_logging(const std::string& categoryname); 
+/// Run the main FCGI processing loop, using @p dispatcher for actual processing.
+/// If @p dispatcher is null, then only dummy processing occurs.
 int mainloop(VerbDispatcher* dispatcher=0);
 
 }  // namespace FCGI
--- src/common/logger.h
+++ src/common/logger.h
@@ -4,6 +4,29 @@ All rights reserved. See file LICENSE for exact terms (2-clause BSD license).
 
 Adriaan de Groot <groot@kde.org>
 */
+
+/**
+ * Logging support for Steamworks. Logging is based on log4cpp, which is
+ * in turn inspired by log4j, and which resembles Python's logging module
+ * as well.
+ *
+ * The Logging::Manager class can be used to read configuration files and
+ * set up logging; without a manager, whatever the default configuration
+ * is, gets used. Logging itself goes through the Logging::getLogger()
+ * function, which returns a logger instance.
+ *
+ * Logging typically goes like this:
+ * 	Logger& log = getLogger("category.subcategory.specifics");
+ * 	log.debugStream() << "A debug message";
+ *
+ * If NDEBUG is defined, logging is a no-op.
+ *
+ * Right now, logging is based on log4cpp. Using only the interfaces in
+ * this file makes it possible to swap log4cpp out for something else:
+ * there are several logging frameworks, and it's not entirely clear
+ * if log4cpp is still maintained.
+ */
+
 #ifndef NDEBUG
 #include <log4cpp/Category.hh>
 #include <log4cpp/PropertyConfigurator.hh>
@@ -17,11 +40,19 @@ namespace Steamworks
 namespace Logging
 {
 #ifdef NDEBUG
+/*
+ * In the NDEBUG case, we have dummy classes LoggerStream,
+ * Logger, and Manager that do nothing. This API needs to
+ * be kept in-sync with the API in the other branch -- in
+ * particular, the names of LogLevel enums.
+ */
 class LoggerStream
 {
 public:
 	LoggerStream() {};
 
+	// Eat anything when logged; this probably consumes more
+	// than the real implementation does.
 	template<typename T> LoggerStream& operator<<(const T& rhs)
 	{
 		return *this;
@@ -31,14 +62,17 @@ public:
 class Logger
 {
 private:
-    static LoggerStream stream;
+	static LoggerStream stream;
 public:
-    Logger() {};
+	Logger() {};
 
-    LoggerStream& debugStream() { return stream; }
-    LoggerStream& infoStream() { return stream; }
+	// TODO: other levels of streams.
+	LoggerStream& debugStream() { return stream; }
+	LoggerStream& infoStream() { return stream; }
 } ;
 
+// Bogus level numbers; I don't know if these are compatible with
+// log4cpp values in particular.
 typedef enum { DEBUG=10, INFO=20 } LogLevel;
 
 class Manager
@@ -64,19 +98,35 @@ public:
 	}
 } ;
 #else
-/// Alias for the actual logging implementation, since I'm nog totally convinced of log4cpp
+/// Provide an alias for the implementation of Logger.
 typedef log4cpp::Category Logger;
+/// Provide an alias for the implementation of LoggerStream.
 typedef log4cpp::CategoryStream LoggerStream;
 
+/// Provide aliases for logging levels (priorities).
 typedef enum { DEBUG=log4cpp::Priority::DEBUG, INFO=log4cpp::Priority::INFO } LogLevel;
 
+/**
+ * Manages configurations for logging by reading in a configuration file:
+ * with log4cpp, this is a Java-style properties file that sets up logging.
+ * Multiple managers may be created; the configuration is additive.
+ * If any managers are created, then when the last one is destroyed logging
+ * is also stopped.
+ */
 class Manager
 {
 private:
 	bool valid;
 	static int instances;
-	
+
 public:
+	/**
+	 * Create a logging manager and read the @p propertyfile
+	 * for configuration information. If the file cannot
+	 * be read, a message is printed to stderr and logging
+	 * is left unchanged (for the first manager, in whatever
+	 * default state the underlying implementation uses).
+	 */
 	Manager(const std::string& propertyfile) :
 		valid(false)
 	{
@@ -94,7 +144,7 @@ public:
 		instances++;
 		getRoot().debugStream() << "LoggerManager created, #" << instances;
 	}
-	
+
 
 	~Manager()
 	{
@@ -105,6 +155,10 @@ public:
 		}
 	}
 
+	/**
+	 * Gets a named logger (e.g. "app.database.sql"). Use an
+	 * empty name to get the root logger (see also getRoot()).
+	 */
 	static
 	Logger& getLogger(const std::string& categoryname)
 	{
@@ -118,6 +172,10 @@ public:
 		}
 	}
 
+	/**
+	 * Gets the root logger (the one with no category). This is
+	 * the eventual destination of all logging messages.
+	 */
 	static
 	Logger& getRoot()
 	{
@@ -127,7 +185,7 @@ public:
 #endif
 
 // Convenience function, use the Manager-implementation (which depends on NDEBUG).
-// This is a template because it is otherwise multiply-defined.
+// This is a template because it otherwise ends up multiply-defined.
 template<typename T>
 Logger& getLogger(const T& categoryname) { return Manager::getLogger(categoryname); }
 
