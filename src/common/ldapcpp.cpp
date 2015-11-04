5abff76d7314726429ee898faa289598feeb205c
Adriaan de Groot <groot@kde.org>
2015-11-04 14:00:23 +0100

    LDAP: return search results as JSON (including attributes)

--- src/common/ldapcpp.cpp
+++ src/common/ldapcpp.cpp
@@ -229,8 +229,17 @@ public:
 				log.infoStream() << " .. entry dn=" << dn;
 				if (results)
 				{
-					picojson::value v_dn(dn);
-					results->emplace(dn, v_dn);
+					picojson::value::object object;
+					picojson::value v_object(object);
+					results->emplace(dn, v_object);
+
+					picojson::value& placed_obj = results->at(dn);
+					picojson::value::object& placed_map = placed_obj.get<picojson::value::object>();
+
+					picojson::value v(dn);
+					placed_map.emplace(std::string("dn"), v);
+
+					copy_entry(entry, placed_map);
 				}
 				entry = ldap_next_entry(ldaphandle, entry);
 			}
@@ -238,6 +247,49 @@ public:
 		log.infoStream() << " .. search OK.";
 
 		ldap_msgfree(res);
+	}
+
+	void copy_entry(LDAPMessage* entry, picojson::value::object& map)
+	{
+		// TODO: guard against memory leaks
+		BerElement* berp(nullptr);
+		char *attr = ldap_first_attribute(ldaphandle, entry, &berp);
+
+		while (attr != nullptr)
+		{
+			berval** values = ldap_get_values_len(ldaphandle, entry, attr);
+			auto values_len = ldap_count_values_len(values);
+			if (values_len == 0)
+			{
+				// TODO: can this happen?
+				picojson::value v_attr;
+				map.emplace(attr, v_attr);  // null
+			}
+			else if (values_len > 1)
+			{
+				// FIXME: decode ber-values
+				picojson::value::array v_array;
+				v_array.reserve(values_len);
+				for (decltype(values_len) i=0; i<values_len; i++)
+				{
+					picojson::value v_attr(values[i]->bv_val);
+					v_array.emplace_back(v_attr);
+				}
+			}
+			else
+			{
+				// FIXME: decode ber-values
+				picojson::value v_attr(values[0]->bv_val);
+				map.emplace(attr, v_attr);
+			}
+			ldap_value_free_len(values);
+			attr = ldap_next_attribute(ldaphandle, entry, berp);
+		}
+
+		if (berp)
+		{
+			ber_free(berp, 0);
+		}
 	}
 } ;
 
