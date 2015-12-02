3c8d86f6ec1d2e2dfaf541185834c042106a447b
Adriaan de Groot <groot@kde.org>
2015-12-02 10:32:53 +0100

    CMake: add FCGI_PREFIX to search for locally-installed FCGI (for instance, on OpenSUSE which has no package for it)

--- cmake/FindFCGI.cmake
+++ cmake/FindFCGI.cmake
@@ -4,16 +4,21 @@
 # Adriaan de Groot <groot@kde.org>
 
 # Try to find FastCGI server libraries. Sets standard variables
-# FCGI_LIBRARIES and FCGI_INCLUDE_DIRS.
+# FCGI_LIBRARIES and FCGI_INCLUDE_DIRS. If there is no system
+# FastCGI package installed, set FCGI_PREFIX to the prefix where
+# FastCGI is installed -- this cmake module will look
+# in lib/ and include/ under that prefix.
 #
 include(FindPackageHandleStandardArgs)
 
-find_library(_FCGI fcgi)
+find_library(_FCGI fcgi
+ PATHS ${FCGI_PREFIX}/lib64 ${FCGI_PREFIX}/lib32 ${FCGI_PREFIX}/lib)
 if (_FCGI)
   set(FCGI_LIBRARIES ${_FCGI})
 endif()
 
-find_path(FCGI_INCLUDE_DIRS fcgi_stdio.h)
+find_path(FCGI_INCLUDE_DIRS fcgi_stdio.h
+  PATHS ${FCGI_PREFIX}/include)
 
 find_package_handle_standard_args(FCGI 
   REQUIRED_VARS FCGI_LIBRARIES FCGI_INCLUDE_DIRS)
