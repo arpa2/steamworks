90a8a502afe555dc7de583bd4d779be4d1712acc
Adriaan de Groot <groot@kde.org>
2016-02-24 10:34:08 +0100

    Common: factor out simple JSON response-handling

---
+++ src/common/jsonresponse.cpp
@@ -0,0 +1,29 @@
+/*
+Copyright (c) 2014-2016 InternetWide.org and the ARPA2.net project
+All rights reserved. See file LICENSE for exact terms (2-clause BSD license).
+
+Adriaan de Groot <groot@kde.org>
+*/
+
+#include "jsonresponse.h"
+
+#include <string>
+
+void Steamworks::JSON::simple_output(Steamworks::JSON::Object response, int status, const char* message, const int err)
+{
+	if (status)
+	{
+		picojson::value status_v(static_cast<double>(status));
+		response.emplace(std::string("status"), status_v);
+	}
+	if ((message != nullptr) && (strlen(message) > 0))
+	{
+		picojson::value msg_v{std::string(message)};
+		response.emplace(std::string("message"), msg_v);
+	}
+	if (errno)
+	{
+		picojson::value errno_v{static_cast<double>(err)};
+		response.emplace(std::string("errno"), errno_v);
+	}
+}
---
+++ src/common/jsonresponse.h
@@ -0,0 +1,32 @@
+/*
+Copyright (c) 2014-2016 InternetWide.org and the ARPA2.net project
+All rights reserved. See file LICENSE for exact terms (2-clause BSD license).
+
+Adriaan de Groot <groot@kde.org>
+*/
+
+#ifndef STEAMWORKS_COMMON_JSONRESPONSE_H
+
+#include "picojson.h"
+
+namespace Steamworks
+{
+namespace JSON
+{
+typedef picojson::value& Values;
+typedef picojson::value::object& Object;
+
+/**
+	* Produce a simple JSON output with the status code and message.
+	* The response is given attributes "status", "message" and "errno".
+	*
+	* This does not change the HTTP status of the FCGI response.
+	*
+	* @see simple_output in fcgi.cpp
+	*/
+void simple_output(Object response, int status, const char* message=nullptr, const int err=0);
+
+}  // namespace JSON
+}  // namespace Steamworks
+
+#endif
