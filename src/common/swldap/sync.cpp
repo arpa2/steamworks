30b6a18f8c34bfc570a8ccda66b11159e5eeacdb
Adriaan de Groot <groot@kde.org>
2016-02-24 14:30:15 +0100

    LDAP: wrestle with adding server controls

--- src/common/swldap/sync.cpp
+++ src/common/swldap/sync.cpp
@@ -6,6 +6,8 @@ Adriaan de Groot <groot@kde.org>
 */
 
 #include "sync.h"
+
+#include "serverinfo.h"
 #include "private.h"
 
 #include "picojson.h"
@@ -44,19 +46,54 @@ void Steamworks::LDAP::SyncRepl::execute(Connection& conn, Result results)
 
 	Steamworks::Logging::Logger& log = Steamworks::Logging::getLogger("steamworks.ldap");
 
+	log.debugStream() << "Server controls are set:";
+	::LDAPControl** ctl = server_controls(conn);
+	::LDAPControl** ctl_i = ctl;
+
+	unsigned int ctl_count = 0;
+	while (*ctl_i)
+	{
+		log.debugStream() << " .. control " << (void *)*ctl_i << " " << (*ctl_i)->ldctl_oid;
+		ctl_i++;
+		ctl_count++;
+	}
+
+	// TODO: check if syncrepl is already enabled
+
+	// Allocate room for an extra ptr (+2 because of the NULL ptr at the end which wasn't counted).
+	::LDAPControl** qctl = (::LDAPControl**)calloc(ctl_count+2, sizeof(::LDAPControl*));
+
+	// Copy controls and double-null-terminate
+	::LDAPControl** qctl_i = qctl;
+	ctl_i = ctl;
+	while (*ctl_i)
+	{
+		*qctl_i++ = *ctl_i++;
+	}
+	*qctl_i = NULL;  // This is where we'll add the new control
+	*(qctl_i+1) = NULL;
+
+	int r = ldap_control_create(ServerControlInfo::SC_SYNC, 1, NULL, 0, qctl_i);
+	if (r)
+	{
+		log.errorStream() << "Control result " << r << " " << ldap_err2string(r);
+		// ignore
+	}
+
+
 	// TODO: settings for timeouts?
 	struct timeval tv;
 	tv.tv_sec = 2;
 	tv.tv_usec = 0;
 
 	LDAPMessage* res;
-	int r = ldap_search_ext_s(ldaphandle,
+	r = ldap_search_ext_s(ldaphandle,
 		d->base().c_str(),
 		LDAP_SCOPE_SUBTREE,
 		d->filter().c_str(),
 		nullptr,  // attrs
 		0,
-		server_controls(conn),
+		qctl,
 		client_controls(conn),
 		&tv,
 		1024*1024,
