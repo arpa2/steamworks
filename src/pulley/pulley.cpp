19fc999e0218d324642688de178414b0f9d07fe0
Adriaan de Groot <groot@kde.org>
2016-04-13 12:32:47 +0200

    Pulley: refactor do_connect()

--- src/pulley/pulley.cpp
+++ src/pulley/pulley.cpp
@@ -49,40 +49,16 @@ int PulleyDispatcher::exec(const std::string& verb, const Values values, Object 
 
 int PulleyDispatcher::do_connect(const Values values, Object response)
 {
+	Steamworks::Logging::Logger& log = Steamworks::Logging::getLogger("steamworks.pulley");
 	std::string name = values.get("uri").to_str();
 
-	Steamworks::Logging::Logger& log = Steamworks::Logging::getLogger("steamworks.pulley");
-	if (name.empty())
-	{
-		log.warnStream() << "No Server URI given to connect.";
-		Steamworks::JSON::simple_output(response, 400, "No server given", LDAP_OPERATIONS_ERROR);
-		return 0;
-	}
-	log.debugStream() << "Connecting to " << name;
-
-	d->connection.reset(new Steamworks::LDAP::Connection(name));
-	if (d->connection->is_valid())
+	if (Steamworks::LDAP::do_connect(d->connection, name, response, log) &&
+	    Steamworks::LDAP::require_syncrepl(d->connection, response, log)
+	)
 	{
 		m_state = connected;
-		Steamworks::LDAP::ServerControlInfo info;
-		info.execute(*d->connection);
-
-		if (!info.is_available(info.SC_SYNC))
-		{
-			m_state = disconnected;
-			d->connection.reset(nullptr);
-			log.warnStream() << "Server at " << name << " does not support SyncRepl.";
-			Steamworks::JSON::simple_output(response, 501, "Server does not support SyncRepl", LDAP_UNAVAILABLE_CRITICAL_EXTENSION);
-			return 0;
-		}
 	}
-	else
-	{
-		log.warnStream() << "Could not connect to " << name;
-		// Still return 0 because we don't want the FCGI to stop.
-		Steamworks::JSON::simple_output(response, 404, "Could not connect to server", LDAP_OPERATIONS_ERROR);
-		return 0;
-	}
+	// Always return 0 because we don't want the FCGI to stop.
 	return 0;
 }
 
