e4c40bbf7a271d576a1cf0f2f8e71830a5f45782
Adriaan de Groot <groot@kde.org>
2016-05-11 13:22:57 +0200

    FCGI: use the bundled library exclusively (since we need to break its event loop)

--- cmake/FindFCGI.cmake
+++ cmake/FindFCGI.cmake
@@ -3,23 +3,8 @@
 #
 # Adriaan de Groot <groot@kde.org>
 
-# Try to find FastCGI server libraries. Sets standard variables
-# FCGI_LIBRARIES and FCGI_INCLUDE_DIRS. If there is no system
-# FastCGI package installed, set FCGI_PREFIX to the prefix where
-# FastCGI is installed -- this cmake module will look
-# in lib/ and include/ under that prefix.
-#
-include(FindPackageHandleStandardArgs)
-
-find_library(_FCGI fcgi
- PATHS ${FCGI_PREFIX}/lib64 ${FCGI_PREFIX}/lib32 ${FCGI_PREFIX}/lib)
-if (_FCGI)
-  set(FCGI_LIBRARIES ${_FCGI})
-endif()
-
-find_path(FCGI_INCLUDE_DIRS fcgi_stdio.h
-  PATHS ${FCGI_PREFIX}/include)
-
-find_package_handle_standard_args(FCGI 
-  REQUIRED_VARS FCGI_LIBRARIES FCGI_INCLUDE_DIRS)
+# Use bundled FCGI
+set(FCGI_LIBRARIES fcgi)
+set(FCGI_INCLUDE_DIRS ${CMAKE_TOP_SOURCE_DIR}/3rdparty/fcgi-2.4.0/include)
+set(FCGI_FOUND TRUE)
 
