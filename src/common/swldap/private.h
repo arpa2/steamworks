a378a45348e98dd7f20d6a9b8e856924ca782e0d
Adriaan de Groot <groot@kde.org>
2016-06-17 09:59:33 +0200

    Common: document some of the internal support functions.

--- src/common/swldap/private.h
+++ src/common/swldap/private.h
@@ -6,7 +6,7 @@ Adriaan de Groot <groot@kde.org>
 */
 
 /**
- * Private implementation details.
+ * Private implementation details, also support functions.
  */
 
 #ifndef SWLDAP_PRIVATE_H
@@ -25,7 +25,18 @@ namespace Steamworks
 namespace LDAP
 {
 
+/**
+ * Copy the DIT entry contained in @p entry into the JSON object @p results,
+ * with attribute names as keys in @p results (e.g. "dn" is a key) and
+ * null, string or array values for each attribute.
+ */
 void copy_entry(::LDAP* ldaphandle, ::LDAPMessage* entry, Result results);
+/**
+ * Copy the DIT entries contained in @p res into the JSON object @p results,
+ * logging to @p log as needed. The DN of each entry is used as key in
+ * @p results, and each value is a JSON object filled as described in
+ * copy_entry().
+ */
 void copy_search_result(::LDAP* ldaphandle, ::LDAPMessage* res, Result results, Logging::Logger& log);
 
 }  // namespace LDAP
