c2685b6f6a1a3962817f1984f9a50f35366ac4d6
Adriaan de Groot <groot@kde.org>
2016-06-19 19:37:16 +0200

    LDAP: stub an update mechinism (for crank)

---
+++ src/common/swldap/update.cpp
@@ -0,0 +1,12 @@
+/*
+Copyright (c) 2016 InternetWide.org and the ARPA2.net project
+All rights reserved. See file LICENSE for exact terms (2-clause BSD license).
+
+Adriaan de Groot <groot@kde.org>
+*/
+
+#include "update.h"
+
+#include "private.h"
+
+#include "picojson.h"
---
+++ src/common/swldap/update.h
@@ -0,0 +1,19 @@
+/*
+Copyright (c) 2016 InternetWide.org and the ARPA2.net project
+All rights reserved. See file LICENSE for exact terms (2-clause BSD license).
+
+Adriaan de Groot <groot@kde.org>
+*/
+
+/**
+ * LDAP operations in a C++ jacket.
+ *
+ * Update a single DIT entry, with or without precondition-checking,
+ * with or without automatic rollback, and optionally with explicit
+ * rollback.
+ */
+
+#ifndef SWLDAP_UPDATE_H
+#define SWLDAP_UPDATE_H
+
+#endif
