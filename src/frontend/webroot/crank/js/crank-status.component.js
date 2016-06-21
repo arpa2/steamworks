d3fee70ad8de11462f978ff52d9cb74fd606c2dc
Adriaan de Groot <groot@kde.org>
2016-06-21 22:20:32 +0200

    Front: example of using $http.post to interact with the FCGI side of the crank

--- src/frontend/webroot/crank/js/crank-status.component.js
+++ src/frontend/webroot/crank/js/crank-status.component.js
@@ -4,7 +4,11 @@ angular.
   module('crankApp').
   component('crankStatus', {
     templateUrl: 'tmpl/crank-status.template.html',
-    controller: function CrankStatusController() {
+    controller: function CrankStatusController($http) {
       this.status = 'Not connected';
+      this.serverstatus = undefined;
+      
+      var self = this;
+      $http.post('/cgi-bin/', {verb: 'serverinfo'}).then(function(response) { self.serverstatus = response.data; });
     }
   });
