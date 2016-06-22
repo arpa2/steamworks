3c68b4f53e7657b441e1ef8f255f64e0016a5260
Adriaan de Groot <groot@kde.org>
2016-06-22 09:53:39 +0200

    Front: add routing (with silly templates for add/view)

--- src/frontend/webroot/crank/js/crank.js
+++ src/frontend/webroot/crank/js/crank.js
@@ -1,5 +1,23 @@
 'use strict';
 
-var app = angular.module('crankApp', []);
+var app = angular.module('crankApp', ['ngRoute']);
 
 app.constant('config', {basecgi: '/cgi-bin/'});
+
+app.config(['$locationProvider', '$routeProvider',
+    function config($locationProvider, $routeProvider) {
+      $locationProvider.hashPrefix('!');
+
+      $routeProvider.
+        when('/certificates', {
+          template: '<crank-certificate-list></crank-certificate-list>'
+        }).
+        when('/certificates/view/:certdn', {
+          template: '<p>Derp</p>'
+        }).
+        when('/certificates/add', {
+          template: '<p>Add</p>'
+        }).
+        otherwise('/certificates');
+    }
+  ]);
