aeabe772ce9fa55a59205b26cae5c4bd59359abe
Adriaan de Groot <groot@kde.org>
2016-06-24 08:41:52 +0200

    Front: view TI data

--- src/frontend/webroot/crank/app/issuer/crank-issuer-view.component.js
+++ src/frontend/webroot/crank/app/issuer/crank-issuer-view.component.js
@@ -7,5 +7,22 @@ angular.
     controller: function CrankIssuerListController($http, $routeParams, config) {
       this.status = false;
       this.issuerdn = $routeParams.issuerdn;
+      this.issuerdata = undefined;
+      
+      var self = this;
+      $http.post(config.basecgi, {
+        verb: 'search',
+        base: this.issuerdn,
+        filter: 'objectClass=tlsPoolTrustedIssuer'}).
+      then(
+        function(response) {
+          self.issuerdata = response.data[self.issuerdn];
+          self.status = true;
+        },
+        function(response) {
+          self.issuerdata = undefined;
+          self.status = false;
+      })
+
     }
   });
