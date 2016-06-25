cc0e7c1ee701d57402affd53b56431a70e42e2f7
Adriaan de Groot <groot@kde.org>
2016-06-25 09:33:24 +0200

    Pulleyscript: expand simple (pulleyscript demo) to read a file from command-line

--- src/pulley/pulleyscript/simple.c
+++ src/pulley/pulleyscript/simple.c
@@ -6,9 +6,24 @@ int main(int argc, char **argv)
 {
 	struct parser prs;
 	int prsret = 0;
+	FILE *fh;
+
+	if (argc < 2)
+	{
+		fprintf(stderr,"Usage: simple <file>\n");
+		return 1;
+	}
 
 	pulley_parser_init(&prs);
-	prsret = pulley_parser_buffer (&prs, PARSER_LINE, "# hi\n", 5);
+
+	fh = fopen (argv [1], "r");
+	if (!fh) {
+		fprintf (stderr, "Failed to open %s\n", argv [1]);
+		return 1;
+	}
+	printf ("Loading %s\n", argv [1]);
+	prsret = pulley_parser_file (&prs, fh);
+	fclose (fh);
 
 	printf("Parser status %d", prsret);
 	return 0;
