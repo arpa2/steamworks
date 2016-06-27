4d71035fbe7154e0efcb4e6e78c6c6375c8ccd84
Adriaan de Groot <groot@kde.org>
2016-06-27 07:20:33 +0200

    Pulleyscript: stub (totally empty) part for decoding binding-data from the compiler.

---
+++ src/pulley/pulleyscript/bindingpp.cpp
@@ -0,0 +1,10 @@
+/*
+Copyright (c) 2016 InternetWide.org and the ARPA2.net project
+All rights reserved. See file LICENSE for exact terms (2-clause BSD license).
+
+Adriaan de Groot <groot@kde.org>
+*/
+
+#include "bindingpp.h"
+
+#include <logger.h>
---
+++ src/pulley/pulleyscript/bindingpp.h
@@ -0,0 +1,24 @@
+/*
+Copyright (c) 2016 InternetWide.org and the ARPA2.net project
+All rights reserved. See file LICENSE for exact terms (2-clause BSD license).
+
+Adriaan de Groot <groot@kde.org>
+*/
+
+/**
+ * This is a C++ wrapper around the PulleyScript parser / generator / etc.
+ * complex. It handles the bindings "machine language".
+ */
+#ifndef STEAMWORKS_PULLEYSCRIPT_BINDINGPP_H
+#define STEAMWORKS_PULLEYSCRIPT_BINDINGPP_H
+
+namespace SteamWorks
+{
+
+namespace PulleyScript
+{
+
+}  // namespace PulleyScript
+}  // namespace
+
+#endif
