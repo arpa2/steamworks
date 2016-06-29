8088e046ff4f40969eb3f637cfb1125f7e720253
Adriaan de Groot <groot@kde.org>
2016-06-29 10:37:06 +0200

    Pulleyscript: massage simple example program for command-line arguments and preparations for plugin-loading

--- src/pulley/pulleyscript/simple.cpp
+++ src/pulley/pulleyscript/simple.cpp
@@ -1,30 +1,128 @@
+#include <getopt.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string>
 
 #include "parserpp.h"
 
 #include <logger.h>
 
-static const char* copyright = "Copyright (C) 2014-2016 InternetWide.org and the ARPA2.net project";
+static const char progname[] = "PulleyScript";
+static const char version[] = "v0.1";
+static const char copyright[] = "Copyright (C) 2014-2016 InternetWide.org and the ARPA2.net project";
+
+static void version_info()
+{
+	printf("SteamWorks %s %s\n", progname, version);
+	printf("%s\n", copyright);
+}
+
+static void version_usage()
+{
+	printf(R"(
+Usage:
+    pulley [-L libdir] scriptfile [...]
+\n\n)");
+}
+
+static void version_help()
+{
+	version_info();
+	printf(R"(
+Use the Pulley to output configuration received from other
+SteamWorks components to a local configuration though the
+backends available on the system.
+
+Backend plug-ins will be loaded from <libdir>. The PulleyScript
+scripts are read once, at start-up.
+
+)");
+	version_usage();
+}
+
 
 int main(int argc, char **argv)
 {
-	if (argc < 2)
+	SteamWorks::Logging::Manager logManager("pulleyscript.properties", SteamWorks::Logging::DEBUG);
+	SteamWorks::Logging::getRoot().debugStream() << "SteamWorks " << progname << ' ' << copyright;
+
+	auto& log = SteamWorks::Logging::getLogger("steamworks.pulleyscript");
+
+	const struct option longopts[] =
 	{
-		fprintf(stderr,"Usage: simple <file>\n");
+		{"version",   no_argument,        0, 'v'},
+		{"help",      no_argument,        0, 'h'},
+		{"libdir",    required_argument,  0, 'L'},
+		{0,0,0,0},
+	};
+
+
+	bool carry_on = true;
+	int index;
+	int iarg = 0;
+
+	while (iarg != -1)
+	{
+		iarg = getopt_long(argc, argv, "L:vh", longopts, &index);
+
+		switch (iarg)
+		{
+		case 'v':
+			version_info();
+			carry_on = false;
+			break;
+		case 'h':
+			version_help();
+			carry_on = false;
+			break;
+		case 'L':
+			log.debugStream() << "-L" << optarg;
+			break;
+		case '?':
+			carry_on = false;
+			break;
+		case -1:
+			// End of options, detected next time around
+			break;
+		default:
+			abort();
+		}
+	}
+	if (!carry_on)
+	{
+		// Either --help or --version or an option error
 		return 1;
 	}
 
-	SteamWorks::Logging::Manager logManager("pulleyscript.properties", SteamWorks::Logging::DEBUG);
-	SteamWorks::Logging::getRoot().debugStream() << "Steamworks PulleyScript " << copyright;
-
 	SteamWorks::PulleyScript::Parser prs;
-	int prsret = prs.read_file(argv[1]);
-	printf("Parser status %d\n", prsret);
-	printf("Parser analysis %d\n", prs.structural_analysis());
-	printf("  .. %s\n", prs.state_string().c_str());
-	printf("Parser SQL setup %d\n", prs.setup_sql());
-	printf("  .. %s\n", prs.state_string().c_str());
+	int prsret = -1;
+	if (!(optind < argc))
+	{
+		version_usage();
+		return 1;
+	}
+	while (optind < argc)
+	{
+		prsret = prs.read_file(argv[optind++]);
+		if (prsret)
+		{
+			//Error in parsing file
+			break;
+		}
+	}
+	if (prsret)
+	{
+		// Couldn't parse file, or no files given.
+		return 1;
+	}
+
+	log.debugStream() << "Parser status " << prsret;
+	prsret = prs.structural_analysis();
+	log.debugStream() << "Parser analysis " << prsret;
+	log.debugStream() << "  .. " << prs.state_string().c_str();
+	prsret = prs.setup_sql();
+	log.debugStream() << "Parser SQL setup " << prs.setup_sql();
+	log.debugStream() << "  .. " << prs.state_string().c_str();
 	prs.explain();
 	return 0;
 }
