e8a74a12f0921d124ab153af77b51bb9048f8364
Adriaan de Groot <groot@kde.org>
2016-07-01 01:14:32 +0200

    PulleyScript: reach the bit where the actual algorithm goes, mark it as TODO in pseudocode

--- src/pulley/pulleyscript/parserpp.cpp
+++ src/pulley/pulleyscript/parserpp.cpp
@@ -35,7 +35,10 @@ public:
 	bool open(struct parser* prs)
 	{
 		auto& log = SteamWorks::Logging::getLogger("steamworks.pulleyscript");
-		log.debugStream() << "Opening SQL for " << prs->scanhash;
+		auto stream = log.debugStream();
+		hash_t h = prs->scanhash;
+		stream << "Opening SQL for ";
+		SteamWorks::Logging::log_hex(stream, (uint8_t *)&h, sizeof(h));
 
 		m_sql = squeal_open_in_dbdir (prs->scanhash, gentab_count (prs->gentab), drvtab_count (prs->drvtab), "/tmp/");
 		return m_sql != nullptr;
@@ -392,6 +395,18 @@ void SteamWorks::PulleyScript::Parser::Private::remove_entry(const std::string& 
 		auto stream = log.debugStream();
 		stream << "  .. generator " << i << " hash ";
 		SteamWorks::Logging::log_hex(stream, (uint8_t *)&h, sizeof(h));
+
+		// TODO: deletion algoritme
+		//
+		// - heeft tabel gen_<hash> deze UUID?
+		//   - ja, levert SQL rijen op (->rij)
+		//     - voor elke backend (->b)
+		//       - roep backend aan voor (g, b) om SQL te krijgen (?)
+		//       - execute SQL met parameter rij (->rij')
+		//         - decrement use-count voor hash(rij')
+		//         - indien use-count 0, backend delete
+		//   - delete all rows w/ UUID
+
 	}
 }
 
@@ -408,5 +423,17 @@ void SteamWorks::PulleyScript::Parser::Private::add_entry(const std::string& uui
 		auto stream = log.debugStream();
 		stream << "  .. generator " << i << " hash ";
 		SteamWorks::Logging::log_hex(stream, (uint8_t *)&h, sizeof(h));
+
+		// TODO: addition algorithm
+		//
+		// - generate list of tuples (a1) x (a2) x .. x (an)
+		//   where (ai) is the set of all values attribute ai
+		//   takes in @p data (->rij)
+		//   - insert UUID, rij into table gen_<hash>
+		//   - voor elke backend (->b)
+		//     - roep backend aan voor (g, b) om SQL te krijgen (?)
+		//     - execute SQL met parameter rij (->rij')
+		//       - increment use-count voor hash(rij')
+		//       - indien use-count == 1, backend add
 	}
 }
